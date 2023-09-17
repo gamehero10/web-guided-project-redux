80fa8f23398ccafa2a86903f54a69b735fd959d9
Rafael Deluna <delunarafael@yahoo.com>
2023-09-17 01:16:47 -0400

    Completed Sprint 10 Module 2 Web Guided Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+import {createStore} from 'redux';
+import { rootReducer} from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 function App() {
   return (
@@ -15,4 +20,9 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+rootElement);
+ 
